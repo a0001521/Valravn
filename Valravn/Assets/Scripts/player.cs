32b0da425ec3d102b0e8af36aa587a98662da690
a0001521 <a0001521@users.noreply.github.com>
2018-06-06 01:16:29 +0800

    Project setup for Github
    The Valravn has now been setup for Github

---
+++ Valravn/Assets/Scripts/player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+    Rigidbody2D playership;
+    public float horsepower;
+    Vector2 steeringpower;
+	// Use this for initialization
+	void Start () {
+        playership = gameObject.GetComponent<Rigidbody2D>();
+        steeringpower = new Vector2(0.0F, 1.0F);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+            playership.AddRelativeForce(steeringpower * horsepower);
+        if (Input.GetKey(KeyCode.S))
+            playership.AddRelativeForce(Vector2.down * horsepower);
+        if (Input.GetKey(KeyCode.A))
+            playership.AddTorque(1);
+        if (Input.GetKey(KeyCode.D))
+            playership.AddTorque(-1);
+    }
+}
