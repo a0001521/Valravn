1e0869557fa605a37c7412c3901b7062bdd29873
a0001521 <a0001521@users.noreply.github.com>
2018-06-27 01:14:47 +0800

    Simple turret traverse behavior
    A simple turret traverse behavior

--- Valravn/Assets/Scripts/turret.cs
+++ Valravn/Assets/Scripts/turret.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class turret : MonoBehaviour {
+    public float speed = 0.75f;
 	// Use this for initialization
 	void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 mousePos = Input.mousePosition;
-        print(mousePos);
+        Vector2 mouseDir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position; //This line accquires the mouse position relative to the object.
+        float angle = (Mathf.Atan2(mouseDir.y, mouseDir.x) * Mathf.Rad2Deg) + 270; //This line converts the data into degrees
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed);
 	}
 }
